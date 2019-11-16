5163012a467603d7743a47d7d0a05d5c75f12d43
mingjing tang <tmjcsh@gmail.com>
2019-11-16 12:20:57 -0500

    creat userRole controller

---
+++ users-api/src/main/java/com/example/usersapi/controller/UserRoleController.java
@@ -0,0 +1,22 @@
+package com.example.usersapi.controller;
+
+import com.example.usersapi.model.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/role")
+public class UserRoleController {
+    @Autowired
+    UserRoleService roleService;
+
+    @PostMapping
+    public UserRole createRole(@RequestBody UserRole role) {
+        return roleService.createRole(role);
+    }
+
+//    @GetMapping("/{rolename}")
+//    public UserRole getRole(@PathVariable String rolename) {
+//        return roleService.getRole(rolename);
+//    }
+}
