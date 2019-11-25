5dc9ff564635bf72423f0f7e937b6e16e1b88309
mingjing tang <tmjcsh@gmail.com>
2019-11-24 19:30:08 -0500

    add ApiOperation to UserProfileController

--- users-api/src/main/java/com/example/usersapi/controller/UserProfileController.java
+++ users-api/src/main/java/com/example/usersapi/controller/UserProfileController.java
@@ -3,7 +3,9 @@ package com.example.usersapi.controller;
 import com.example.usersapi.model.UserProfile;
 import com.example.usersapi.model.wrapper.UserProfileWithUser;
 import com.example.usersapi.service.UserProfileService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,16 +20,19 @@ public class UserProfileController {
   private UserProfileService userProfileService;
 
   @PostMapping("/profile")
+  @ApiOperation(value = "Post a profile of an exist user ", notes = "post profile", response = UserProfileWithUser.class)
   public UserProfileWithUser postUserProfile(@RequestHeader String username, @RequestBody UserProfile userProfile) {
     return userProfileService.createProfile(username, userProfile);
   }
 
   @PutMapping("/profile")
+  @ApiOperation(value = "Update a profile of an exist user ", notes = "update profile", response = UserProfileWithUser.class)
   public UserProfileWithUser updateUserProfile(@RequestHeader String username, @RequestBody UserProfile userProfile){
     return userProfileService.updateProfile(username, userProfile);
   }
 
   @GetMapping("/profile")
+  @ApiOperation(value = "Get a profile of an exist user ", notes = "get profile", response = UserProfileWithUser.class)
   public UserProfileWithUser getUserProfile(@RequestHeader String username){
     return userProfileService.getProfile(username);
   }
