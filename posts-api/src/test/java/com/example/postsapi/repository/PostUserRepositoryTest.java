43e5f2e8b3592b1995bcb227afa8e177185e5c4c
mingjing tang <tmjcsh@gmail.com>
2019-11-26 17:29:11 -0500

    update postUserRepositoryTest

--- posts-api/src/test/java/com/example/postsapi/repository/PostUserRepositoryTest.java
+++ posts-api/src/test/java/com/example/postsapi/repository/PostUserRepositoryTest.java
@@ -13,6 +13,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -33,7 +41,14 @@ public class PostUserRepositoryTest {
     private Comment comment;
 
     @Mock
-    CommentSender commentSender;
+    JdbcTemplate jdbcTemplate;
+
+    @Mock
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @InjectMocks
+    PostUserRepository postUserRepository;
+
 
     private PostWithUser postWithUser;
 
@@ -62,9 +77,43 @@ public class PostUserRepositoryTest {
 
     @Test
     public void save_Success(){
-        when(commentSender.createComment(anyString(), anyLong(), any())).thenReturn(comment);
-        Comment result = commentRepository.createComment("name", 1L, comment);
+        when(jdbcTemplate.update(anyString(), anyLong(), anyLong())).thenReturn(1);
+        int result = postUserRepository.save(1l, 1l);
         assertNotNull(result);
-        assertEquals(result, comment);
+        assertEquals(result, 1L);
     }
+
+    @Test
+    public void getUserIdByPostId_Success(){
+        when(jdbcTemplate.queryForObject(anyString(), (Class<Object>) any())).thenReturn(1L);
+        String sql = "SELECT user_id FROM post_user WHERE post_id = " + 1L;
+        Long result = postUserRepository.getUserIdByPostId(1L);
+        assertNotNull(result);
+        assertEquals((long)result, (long)1L);
+    }
+
+    @Test
+    public void findUserIdsByPostIds_Success(){
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(1L);
+        List<Long> postIds = new ArrayList<>();
+        postIds.add(1L);
+        when(namedParameterJdbcTemplate.query(anyString(), anyMap(), any(RowMapper.class))).thenReturn(userIds);
+        List<Long> result = postUserRepository.findUserIdsByPostIds(postIds);
+        assertNotNull(result);
+        assertEquals(result, userIds);
+    }
+
+    @Test
+    public void findUserIdsByUserIds_Success(){
+        List<Long> postIds = new ArrayList<>();
+        postIds.add(1L);
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn();
+    }
+
+
+//    @Test
+//    public void deleteByPostId_Success(){
+//        when(jdbcTemplate.update(anyString(), anyLong())).thenReturn(null);
+//    }
 }
