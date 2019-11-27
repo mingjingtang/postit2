e8ad61b3473c83fb65b8c9cd139a404432ea4b95
Qiming Chen <qimingchen91129@gmail.com>
2019-11-27 10:26:55 -0500

    remove target

--- users-api/target/classes/db/migration/V4__create_comments.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE comments(
-  id SERIAL UNIQUE NOT NULL,
-  text VARCHAR(255) NOT NULL
-);
--- users-api/target/classes/db/migration/V5__create_post_comment.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE post_comment (
-    post_id INT NOT NULL,
-    comment_id INT UNIQUE NOT NULL
-);
