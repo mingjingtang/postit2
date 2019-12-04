4758399015ded577b79913dd41f681b68fe58d7a
mingjing tang <tmjcsh@gmail.com>
2019-12-03 21:50:37 -0500

    pcf finished

---
+++ posts-api/src/main/java/com/example/postsapi/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.postsapi.config;
+
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        //@formatter:off
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                "X-Requested-With", "Origin", "Content-Type",
+                "Accept", "Authorization", "cache-control"));
+        //@formatter:on
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().permitAll()
+//        .antMatchers("/role/**").hasRole("ADMIN")
+                .and().httpBasic().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//    http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
--- posts-api/src/main/java/com/example/postsapi/config/SwaggerConfig.java
+++ posts-api/src/main/java/com/example/postsapi/config/SwaggerConfig.java
@@ -20,13 +20,13 @@ public class SwaggerConfig {
 
   @Bean
   public Docket api() throws IOException, XmlPullParserException {
-    MavenXpp3Reader reader = new MavenXpp3Reader();
-    Model model = reader.read(new FileReader("pom.xml"));
+//    MavenXpp3Reader reader = new MavenXpp3Reader();
+//    Model model = reader.read(new FileReader("pom.xml"));
     return new Docket(DocumentationType.SWAGGER_2).select()
         .apis(RequestHandlerSelectors.basePackage("com.example.postsapi.controller"))
         .paths(PathSelectors.any()).build().apiInfo(
             new ApiInfo("Posts-Api Documentation", "Documentation automatically generated",
-                model.getParent().getVersion(), null, new Contact("kyle", "", "qc91129@gmail.com"),
+                "1.0", null, new Contact("kyle", "", "qc91129@gmail.com"),
                 null, null));
   }
 }
